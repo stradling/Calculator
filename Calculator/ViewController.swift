34f0b3cc8f28ab7f0d3c4a7de9a8ffd01fae0ea3
Alden Stradling <stradling@gmail.com>
2015-06-02 11:33:19 -0500

    Operators

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -26,5 +26,52 @@ class ViewController: UIViewController {
 
     }
 
+    @IBAction func Operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            enter()
+        }
+        switch operation {
+            case "×": performOperation {$0 * $1}
+            case "÷": performOperation {$1 / $0}
+            case "+": performOperation {$0 + $1}
+            case "−": performOperation {$1 - $0}
+            default: break
+        }
+        
+    }
+   
+    func performOperation(operation:(Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+            
+    }
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
+
+
+
+
+// Characters reference
+// +−÷×
+//→π√␡⏎±
+//sin cos tan
